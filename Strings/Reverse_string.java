7ebd7ce53b291ccf26dbc96632048b27e40f5d16
sahildit <sahilhooda3500@gmail.com>
2023-07-18 00:35:28 +0100

    Reverse String

---
+++ Strings/Reverse_string.java
@@ -0,0 +1,27 @@
+/**
+ * Ref vide0 => https://www.youtube.com/watch?v=eFXcYym2ymo
+ */
+
+
+package Strings;
+
+public class Reverse_string {
+
+    private static String reverse(String str){
+
+        if(str == null || str.length() <=1){
+            return str;
+        }
+
+        return reverse(str.substring(1)) + str.charAt(0);
+        
+    }
+    
+    public static void main(String[] args){
+
+        String str = "object";
+        String revStr = reverse(str);   
+
+        System.out.print(revStr);
+    }
+}
