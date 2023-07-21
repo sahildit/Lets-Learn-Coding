43c53a1a3fe2cbf4d28efaf778d04ee18863df55
sahildit <sahilhooda3500@gmail.com>
2023-07-21 08:31:01 +0100

    Check if any digit present in a string

---
+++ Check_if_digits_in_String.java
@@ -0,0 +1,17 @@
+public class Check_if_digits_in_String {
+    
+    public static void main(String[] args){
+        String str = "this is my 1 test based on assumptions 123";
+        System.out.println(ContainsDigits(str));
+    }
+
+    public static Boolean ContainsDigits(String input){
+
+        // boolean is used so that it will return either true (if any digits) and false(if no digits in string)
+
+        // here ".*\\d+.*" is a rejects patter which is used to find any digit in string -> its just a formula
+
+        // input.matches is used simply to check if our string that we are checking matches the rules defined in for rejects and in this case it finds if there's any digit present or not.
+        return input.matches(".*\\d+.*");
+    }
+}
